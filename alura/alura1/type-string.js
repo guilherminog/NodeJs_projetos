fb3581933b47ef2c41b911f16eae9ee1c9d4002f
Guilhermino Gomes <90943259+redj4ck@users.noreply.github.com>
2021-09-26 18:51:47 -0300

    vscode://vscode.github-authentication/did-authenticate?windowid=1&code=4bae8a0ee37eb70ed387&state=be537523-9009-456d-9e3c-769b667ccb4b

--- alura/alura1/type-string.js
+++ alura/alura1/type-string.js
@@ -3,15 +3,41 @@ const texto2 = "Olá Mundo!";
 const senha = "senhaSuperSegura456!";
 const stringDeNumeros = "34567";
 
-const citacao = 'O Professor disse: "Olá Mundo!"';
+const citacao = 'Meu nome é ';
+const meuNome = 'Guilhermino';
 
-console.log(citacao)
+//console.log(citacao)
 
 // concatenação
 
+//console.log(citacao + meuNome) <===
+
+//template string Ou template literal??????
 
 
 
+//código Unicode
 
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
 
-//template string Ou template literal
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+//  Case Sensetive
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+//quantidade de caracteres
+
+const senha = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
