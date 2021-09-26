43ba45eafe5871f9704b38daf2692558fa0a4f3b
Guilhermino Gomes <90943259+redj4ck@users.noreply.github.com>
2021-09-26 18:19:18 -0300

    first commit

---
+++ alura/alura1/type-nunber.js
@@ -0,0 +1,29 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1
+const segundoNumero = 2
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica)
+
+// ponto flutuante 
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSeZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante
+
+console.log(novaOperacao)
+
+// NaN -> Not A Number (nao e um numero)
+
+const alura = "Alura"
+console.log(alura * primeiroNumero)
+
+// tipo infinity
+ var a = 10
+ var b = 0
+ console.log(a/b)
