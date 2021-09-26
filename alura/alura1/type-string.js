3239bc2faeca75b3aaba4cfc4f286d0099fb4a67
Guilhermino Gomes <90943259+redj4ck@users.noreply.github.com>
2021-09-26 18:34:49 -0300

    vscode://vscode.github-authentication/did-authenticate?windowid=1&code=84e56a1d4c54f1c24d8f&state=469a84f9-e4b0-4dcb-87a4-bc720ad62998

---
+++ alura/alura1/type-string.js
@@ -0,0 +1,6 @@
+const texto1 = 'Olá Mundo!';
+const texto2 = "Olá Mundo!";
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao = 'O Professor disse: "Olá Mundo!"';
