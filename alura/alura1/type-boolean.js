88d38e032e82eff0cb9279a91dc471fddca1a7d7
Guilhermino Gomes <90943259+redj4ck@users.noreply.github.com>
2021-09-26 19:09:29 -0300

    vscode://vscode.github-authentication/did-authenticate?windowid=1&code=4bae8a0ee37eb70ed387&state=be537523-9009-456d-9e3c-769b667ccb4b

---
+++ alura/alura1/type-boolean.js
@@ -0,0 +1,17 @@
+//Boolean
+//Treu or False --> Verdadeiro ou Falso
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true
+
+console.log(primeiroNumero === segundoNumero);
+
+
+
+const texto1 = 'Alura';
+const texto2 =  'alura';
+
+const inputMinusculo = texto1.toLowerCase();
+
+console.log(inputMinusculo === texto2)
