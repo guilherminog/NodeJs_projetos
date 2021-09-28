bbb0c0f0f50526c522221bfba53d30f10622f0d2
Guilhermino Gomes <guilhermino@hotmail.com.br>
2021-09-27 22:29:53 -0300

    vscode://vscode.github-authentication/did-authenticate?windowid=3&code=c29a3d6d3c84df29c566&state=48fe2003-05d1-44ea-a2cb-c16728a97c01

---
+++ alura/alura1/template-strings.js
@@ -0,0 +1,10 @@
+const nome = "Gulhermino";
+const idade = (2021-1986);
+const cidadeDeNascimento = "Rio de Janeiro"
+
+//const apresentacao = "Meu nome é " + nome +", minha idade é " 
+//+ idade + " e minha cidade é " + cidadeDeNascimento + ".";
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade do ${cidadeDeNascimento}.`;
+
+console.log(apresentacao);
